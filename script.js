7d68814327f59ba7106fc7021afe798311b2111c
Trill Ville <computersleek@gmail.com>
2021-01-28 23:28:13 -0500

    added js and updated html

--- script.js
+++ script.js
@@ -1,54 +1,142 @@
-var container = document.querySelector("#container")
-var questions = document.querySelector("#questions")
-var options = document.querySelector("#options")
-var opt1 = document.querySelector("#opt1")
-var opt2 = document.querySelector("#opt2")
-var opt3 = document.querySelector("#opt3")
-var opt4 = document.querySelector("#opt4")
-var score = document.querySelector("#score")
-var start = document.querySelector("#start")
-var next = document.querySelector("#next")
+// Quiz questions
+var questions = [
+    {
+    question: "Who killed Mufasa? ",
+    options: ["Rafiki", "Scar", "Zazu ", "The Hyenas",],
+    answer: "Scar"
+    },
+    {
+    question: "Who killed Scar? ",
+    options: ["Mufasa", "Simba", "The Hyenas", "Zazu ",],
+    answer: "The Hyenas"
+    },
+    {
+    question: "What was Simba's mom's name? ",
+    options: ["Sarabi ", "Nala", "Belle ", "Zaire ",],
+    answer: "Sarabi"
+    },
+    {
+    question: "What actor/actress voice the lead hyena? ",
+    options: ["Jim Carrey ", "Robin Williams ", "Will Smith ", "Whoopi Goldberg ",],
+    answer: "Whoopi Goldberg"
+    },
+    {
+    question: "What were Simba's two male sidekicks (of different species)? ",
+    options: ["Pumba & Timon ", "Bear & Malone ", "Pumba & Timon ", "Zazu & Rafiki",],
+    answer: "Pumba & Timon"
+    }
+    ]
 
-var index = 0
-var questionsList = [
-    {
-        question:"Who killed Mufasa? ",
-        opt1: "Rafiki",
-        opt2: "Scar",
-        opt3: "Zazu ",
-        opt4: "The Hyenas",
-        answer: "Scar"
+
+// var index = 0
+
+// // object variables below
+// var score = 0;
+// var currentQuestion = -1;
+// var timeLeft = 0;
+// var timer;
+
+
+let score = Math.floor(Math.random() * 100);
+// start timer
+function start() {
+    var time = 75;
+    let timeLeft = 0;
+    console.log(time);
+
+    console.log(timeLeft)
+   
+
+    timerClock = setInterval(function() {
+       document.querySelector('.time-left').innerHTML = time;
+        time--;
+         if (time <= -1) {
+            clearInterval(timerClock);
+            endGame(); 
+        }
+    }, 1000);
+    
+}
+
+console.log(start());
+
+// end timer
+function endGame() {
+    clearInterval(timerClock);
+    var quizContent = `
+    <h2>Game over!</h2>
+    <h3>You got a ${score} /100!</h3>
+    <h3>That means you got  + ${score / 100} +   questions correct!</h3>
+    <input type="text" id="name" placeholder="First name"> 
+    <button onclick="setScore()">Set score!</button>
+    `
+    document.getElementById("quiz-two").innerHTML = quizContent;
+}
+
+// set score
+function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName",  document.getElementById('name').value);
+    getScore();
+}
+// get score
+
+function getScore() {
+    var quizDontent = `
+    <h2>  ${localStorage.getItem("highscoreName")}  highscore is:</h2>    
+    <h1>  ${localStorage.getItem("highscore")}   </h1><br> 
+    
+    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+    `;
+    document.getElementById("quiz-two").innerHTML = quizDontent;
+}
+// Clear score
+function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName", "");
+    resetGame();
+}
+
+// Reset Game
+
+function resetGame() {
+    clearInterval(timerClock);
+    let score = 0;
+    let currentQuestion = -1;
+    let timeLeft = 0;
+    let timer = null;
+    document.querySelector(".time-left").innerHTML = timeLeft;
+    var quizContent = `
+    <h1>
+        Lion King Quiz!
+    </h1>
+    <h3>
+        Play Now!   
+    </h3>
+    <button onclick="start()">Start!</button>`;
+    document.getElementById("quiz-two").innerHTML = quizContent;
+}
+
+// 15 sec deduction
+function incorrect() {
+   let timeLeft = 15; 
+    next();
+}
+
+// 20 sec increase
+function correct() {
+    score += 20;
+    next();
+}
+
+console.log(incorrect());
+
+//  Loop questions
+function next() {
+    for(let i = 0)
+    currentQuestion++;
+    if (currentQuestion > questions.length - 1) {
+        endGame();
+        return;
     }
-    {
-        question: "Who killed Scar? "
-        opt1: "Mufasa",
-        opt2: "Simba",
-        opt3: "The Hyenas",
-        opt4: "Zazu ",
-        answer: "The Hyenas"
-    }
-    {
-        question: "What was Simba's mom's name? "
-        opt1: "Sarabi ",
-        opt2: "Nala",
-        opt3: "Belle ",
-        opt4: "Zaire ",
-        answer: "Sarabi"
-    }
-    {
-        question: "What actor/actress voice the lead hyena? "
-        opt1: "Jim Carrey ",
-        opt2: "Robin Williams ",
-        opt3: "Will Smith ",
-        opt4: "Whoopi Goldberg ",
-        answer: "Whoopi Goldberg"
-    }
-    {
-        question: "What were Simba's two male sidekicks (of different species)? "
-        opt1:
-        opt2: "Bear & Malone ",
-        opt3: "Pumba & Timon ",
-        opt4: "Zazu & Rafiki",
-        answer: "Pumba & Timon"
-    }
-]
+}
